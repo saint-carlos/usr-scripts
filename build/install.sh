776672551336c2248b1d8eb1d2f4ddb1a3d094eb
Idan Kedar <idank@tonian.com>
2012-03-18 14:16:04 +0200

    make: refactor install
    move common install code to a function.
    drop the use of '&&' since the script is running with the -e flag.

--- build/install.sh
+++ build/install.sh
@@ -1,15 +1,26 @@
 #!/bin/bash -xe
 
+install_dir()
+{
+	DIRNAME="$1"
+	MODE="$2"
+	PLACEHOLDER="$3"
+	mkdir -p "$PLACEHOLDER"
+	cp "$INSTALL_SOURCE/$DIRNAME"/* "$PLACEHOLDER"
+	chmod "$MODE" "$PLACEHOLDER"/*
+
+}
+
 [ $# -eq 2 ] || exit 1
 
 PARAMS_FILE="$1"
 INSTALL_SOURCE="$2"
 
-. "${PARAMS_FILE}"
+. "$PARAMS_FILE"
 
-mkdir -p "$ETC_PLACEHOLDER" && cp $INSTALL_SOURCE/etc/* $ETC_PLACEHOLDER && chmod 0644 $ETC_PLACEHOLDER/*
-mkdir -p "$BIN_PLACEHOLDER" && cp $INSTALL_SOURCE/bin/* $BIN_PLACEHOLDER && chmod 0755 $BIN_PLACEHOLDER/*
-if [ `id -u` -eq 0 ]; then
-	mkdir -p "$SBIN_PLACEHOLDER" && cp $INSTALL_SOURCE/sbin/* $SBIN_PLACEHOLDER && chmod 0755 $SBIN_PLACEHOLDER/*
+install_dir etc 0644 "$ETC_PLACEHOLDER"
+install_dir bin 0755 "$BIN_PLACEHOLDER"
+if [ $UID -eq 0 ]; then
+	install_dir sbin 0755 "$SBIN_PLACEHOLDER"
 fi
 
