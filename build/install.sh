1dd6b9f68ce1e4dcc5c98affdd39f4e981c625f8
Idan Kedar <idank@primarydata.com>
2015-02-24 16:26:53 -0800

    make: project version in vroot
    Signed-off-by: Idan Kedar <idank@primarydata.com>

--- build/install.sh
+++ build/install.sh
@@ -9,6 +9,9 @@ shift 2
 source "$PARAMS_FILE"
 source "$(dirname $BASH_SOURCE)/common.sh"
 
+mkdir "$CONFIG_VROOT"
+git rev-parse HEAD > "$CONFIG_VROOT/version"
+
 for FILE; do
 	DIR=${FILE%%/*}
 	if [ $UID -ne 0 ] && [ $DIR = sbin ]; then
